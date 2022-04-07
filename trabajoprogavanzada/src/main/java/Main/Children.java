6c9b2727dc39ec7cb26d4a9ac8f702d2d37457fa
HerasZ <78933654+HerasZ@users.noreply.github.com>
2022-04-07 12:24:51 +0200

    Añadida clase children

---
+++ trabajoprogavanzada/src/main/java/Main/Children.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+/**
+ *
+ * @author Heras
+ */
+public class Children extends Thread {
+    
+    private int id;
+    
+    public Children(int startid) {
+        this.id = startid;
+    }
+}
