559ab01b57bad6a399a6b1222e82ff6fe648e64f
Gonzalo Todó Gabino <91533634+Gonseilo@users.noreply.github.com>
2022-05-05 18:23:15 +0200

    Snack en proceso, pido paciencia ;_;

--- trabajoprogavanzada/src/main/java/Main/Snack.java
+++ trabajoprogavanzada/src/main/java/Main/Snack.java
@@ -5,10 +5,61 @@
  */
 package Main;
 
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  *
  * @author Heras
  */
 public class Snack {
+    
+    private LinkedBlockingQueue<Children> snackQueue = new LinkedBlockingQueue<Children>();
+    private LinkedBlockingQueue<String> cleanTrays= new LinkedBlockingQueue<String>();
+    private LinkedBlockingQueue<String> dirtyTrays = new LinkedBlockingQueue<String>();
+    private CommonArea commonArea;
+    private ReentrantLock snackLock;
+    private Semaphore snackCapacity;
+    private  Instructor snackInstructor1;
+    private  Instructor snackInstructor2;
+    
+    
+    
+    public Snack(){
+        snackCapacity = new Semaphore(20,true);
+        snackLock = new ReentrantLock(true);
+    }
+    
+    
+    public void setCommonArea(CommonArea newCommonArea){
+        this.commonArea = newCommonArea;
+    }
+    
+    
+    
+    public void enterSnack(Children newChild){
+        snackQueue.add(newChild);
+        
+    }
+    
+    
+    public void useSnack(){
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
