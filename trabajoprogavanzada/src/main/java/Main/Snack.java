d56e1f8be9c931e1dc7992c25b0b78aafaed9443
HerasZ <78933654+HerasZ@users.noreply.github.com>
2022-05-07 14:12:06 +0200

    arreglando snack

--- trabajoprogavanzada/src/main/java/Main/Snack.java
+++ trabajoprogavanzada/src/main/java/Main/Snack.java
@@ -8,87 +8,90 @@ package Main;
 import static java.lang.Thread.sleep;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Heras
  */
 public class Snack {
-    
+
     private LinkedBlockingQueue<Children> snackQueue = new LinkedBlockingQueue<Children>();
     private CommonArea commonArea;
     private ReentrantLock snackLock;
     private Semaphore snackCapacity;
     private Instructor snackInstructor1;
     private Instructor snackInstructor2;
-    private int cleanTrays = 0;
-    private int dirtyTrays = 25;
+    private AtomicInteger cleanTrays;
+    private AtomicInteger dirtyTrays;
     private boolean empty;
     private Condition pileEmpty;
     private Condition pileFull;
-    
-    
-    
-    public Snack(){
-        snackCapacity = new Semaphore(20,true);
+
+    public Snack() {
+        snackCapacity = new Semaphore(20, true);
         snackLock = new ReentrantLock(true);
+        pileEmpty = snackLock.newCondition();
+        pileFull = snackLock.newCondition();
+        dirtyTrays.set(25);
     }
-    
-    
-    public void setCommonArea(CommonArea newCommonArea){
+
+    public void setCommonArea(CommonArea newCommonArea) {
         this.commonArea = newCommonArea;
-        
     }
-    
-    
-    
-    
-    
-    
-    public void useSnack(Children newChild) throws InterruptedException{
+
+    public void useSnack(Children newChild) {
         snackQueue.add(newChild);
-        snackCapacity.acquire();
-        snackQueue.remove(newChild);
-        snackLock.lock();
-        try{
-            while(empty){
-            try{
+        try {
+            snackCapacity.acquire();
+            snackLock.lock();
+            while (cleanTrays.get() == 0) {
                 pileEmpty.await();
-            }catch(Exception e){}
-                       
-        }
-        empty = false;
-        cleanTrays--;
-        System.out.println(newChild.getIdChild() + " on Snack");
-        sleep(7000);
-        dirtyTrays++;
-        pileFull.signalAll();
-        }catch(Exception  e){}
-        finally{
-            snackQueue.remove(newChild);
+            }
             snackLock.unlock();
+            cleanTrays.getAndDecrement();
+            System.out.println(newChild.getIdChild() + " on Snack");
+            sleep(7000);
+            dirtyTrays.getAndIncrement();
+        } catch (InterruptedException e) {
+        } finally {
+            snackQueue.remove(newChild);
+            snackCapacity.release();
         }
-        snackCapacity.release();
-            
     }
-    
-    
-    
-    public void cleanTrays(Instructor instructor1, Instructor instructor2){
-        System.out.println("Instructor1 on zipline");
-        System.out.println("Instructor2 on zipline");
+
+    public void cleanTrays(Instructor cleaningInstructor) {
+        while (true) {
+            if (cleaningInstructor.getBreakCountdown() <= 0) {
+                //INSTRUCTOR TAKES HIS BREAK
+                try {
+                    System.out.println(cleaningInstructor.getIdInst() + " taking break");
+                    commonArea.instructorBreakBegin(cleaningInstructor);
+                    sleep((int) (1000 + 1000 * Math.random()));
+                    commonArea.instructorBreakOver(cleaningInstructor);
+                    cleaningInstructor.resetBreakCountdown();
+                    System.out.println(cleaningInstructor.getIdInst() + " break over");
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(ZipLine.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            } else {
+                cleaningInstructor.lowerBreakCountdown();
+            }
+        }
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+    public void setSnackInstructor1(Instructor snackInstructor1) {
+        this.snackInstructor1 = snackInstructor1;
+        cleanTrays(this.snackInstructor1);
+    }
+
+    public void setSnackInstructor2(Instructor snackInstructor2) {
+        this.snackInstructor2 = snackInstructor2;
+        cleanTrays(this.snackInstructor2);
+    }
 
 }
